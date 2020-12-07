fa39f1e2db65cf8b6828d627e1a258ea454eb890
Poli Gonzalez <gonzalezpoli10@gmail.com>
2020-12-07 11:55:11 -0600

    PI with GET

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,18 @@
 package com.example.demo;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/")
+
+    @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello from Spring!";
+        return "Hello World";
+    }
+    @GetMapping("/math/pi")
+    public String pie() {
+        return "3.141592653589793";
     }
 
 }
